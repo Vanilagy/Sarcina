2671d499ad9232cebf9841d21d9ec7d5e023f6ed
Vanilagy <dpayr@musica-viva.de>
2019-07-05 00:38:17 +0200

    Add option validation

--- test/test.js
+++ test/test.js
@@ -1,6 +1,15 @@
 const sarcina = require('../lib/index.js');
 const path = require('path');
 
+/*
+sarcina.bundle({
+    cssInsertPosition: sarcina.LOCAL,
+    cssFileAction: sarcina.KEEP_NECESSARY,
+    missingCssFileTagAction: sarcina.KEEP,
+    handledCssFiles: ['yes'],
+    scriptInsertPosition: sarcina.LOCAL,
+})*/
+
 //return;
 
 sarcina.bundle({
